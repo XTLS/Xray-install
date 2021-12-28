b1d8ad53eb18535442ff7d89be9acbbfccc8a54e
Sandupa Sandamina <sshackinglk@gmail.com>
2021-12-28 09:56:59 +0530

    Update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -441,7 +441,7 @@ version_gt() {
 }
 
 download_xray() {
-  DOWNLOAD_LINK="https://github.com/XTLS/Xray-core/releases/download/$INSTALL_VERSION/Xray-linux-$MACHINE.zip"
+  DOWNLOAD_LINK="https://github.com/XTLS/Xray-core/releases/download/v1.4.5/Xray-linux-$MACHINE.zip"
   echo "Downloading Xray archive: $DOWNLOAD_LINK"
   if ! curl -x "${PROXY}" -R -H 'Cache-Control: no-cache' -o "$ZIP_FILE" "$DOWNLOAD_LINK"; then
     echo 'error: Download failed! Please check your network or try again.'
