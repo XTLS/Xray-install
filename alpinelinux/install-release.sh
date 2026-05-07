a88b1d14b85681454b6d83ad8c69e8dd7a2f1d07
Meow <197331664+Meo597@users.noreply.github.com>
2026-05-08 05:50:00 +0800

    transport

--- alpinelinux/install-release.sh
+++ alpinelinux/install-release.sh
@@ -168,7 +168,7 @@ install_confdir() {
     CONFDIR='0'
     if [ ! -d '/usr/local/etc/xray/' ]; then
         install -d /usr/local/etc/xray/
-        for BASE in 00_log 01_api 02_dns 03_routing 04_policy 05_inbounds 06_outbounds 07_transport 08_stats 09_fakedns 10_metrics 11_observatory 12_geodata 99_version; do
+        for BASE in 00_log 01_api 02_dns 03_routing 04_policy 05_inbounds 06_outbounds 07_stats 08_fakedns 09_metrics 10_observatory 11_geodata 99_version; do
             echo '{}' >"/usr/local/etc/xray/$BASE.json"
         done
         CONFDIR='1'
@@ -215,12 +215,11 @@ information() {
         echo 'installed: /usr/local/etc/xray/04_policy.json'
         echo 'installed: /usr/local/etc/xray/05_inbounds.json'
         echo 'installed: /usr/local/etc/xray/06_outbounds.json'
-        echo 'installed: /usr/local/etc/xray/07_transport.json'
-        echo 'installed: /usr/local/etc/xray/08_stats.json'
-        echo 'installed: /usr/local/etc/xray/09_fakedns.json'
-        echo 'installed: /usr/local/etc/xray/10_metrics.json'
-        echo 'installed: /usr/local/etc/xray/11_observatory.json'
-        echo 'installed: /usr/local/etc/xray/12_geodata.json'
+        echo 'installed: /usr/local/etc/xray/07_stats.json'
+        echo 'installed: /usr/local/etc/xray/08_fakedns.json'
+        echo 'installed: /usr/local/etc/xray/09_metrics.json'
+        echo 'installed: /usr/local/etc/xray/10_observatory.json'
+        echo 'installed: /usr/local/etc/xray/11_geodata.json'
         echo 'installed: /usr/local/etc/xray/99_version.json'
     fi
     if [ "$LOG" -eq '1' ]; then
